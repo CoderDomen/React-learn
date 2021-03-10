559f7471ab93cb63706cec388d1223abd9b96be0
coder <942860706@qq.com>
2021-03-10 11:51:55 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import APP from './APP.jsx'
-
-ReactDOM.render(<APP />,document.getElementById('root'))
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import APP from "./App.jsx";
+import APP from "./APP.jsx";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -18,4 +10,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
->>>>>>> b395f0a6ba2313dcfb6efa4058f7b76e747e791a
